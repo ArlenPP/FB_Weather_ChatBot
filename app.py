b827e090e8e2f78cf34c9bb4444bf0fb8a958bc9
yiiju <yijuhung0129@gmail.com>
2018-12-17 02:17:15 +0800

    Change to TA code to test heruko

--- app.py
+++ app.py
@@ -1,16 +1,13 @@
-from bottle import Bottle, route, run, request, abort, static_file
-
-from fsm import TocMachine
-import states_config
-import global_config
 import os
+from bottle import Bottle, request, abort, static_file
+import send
 
 app = Bottle()
 
-VERIFY_TOKEN = os.environ.get("FB_VERIFY_TOKEN")
+VERIFY_TOKEN = os.environ['FB_VERIFY_TOKEN']
 PORT = os.environ['PORT']
 
-@app.route("/yiju", method="GET")
+@app.route("/webhook", method="GET")
 def setup_webhook():
     mode = request.GET.get("hub.mode")
     token = request.GET.get("hub.verify_token")
@@ -19,37 +16,24 @@ def setup_webhook():
     if mode == "subscribe" and token == VERIFY_TOKEN:
         print("WEBHOOK_VERIFIED")
         return challenge
+
     else:
         abort(403)
 
-@app.route("/yiju", method="POST")
+@app.route("/webhook", method="POST")
 def webhook_handler():
     body = request.json
-    print('\nFSM STATE: ' + machine.state)
+    print('REQUEST BODY: ')
+    print(body)
 
     if body['object'] == "page":
         event = body['entry'][0]['messaging'][0]
-        sender_id = event['sender']['id']
-
-        if sender_id in global_config.id_list[0]:
-            global_config.get_state(sender_id)
-            if machine.state == 'state_init':
-                machine.go_to(event)
-            else:
-                machine.advance(event)
-        else:
-            global_config.set_id(sender_id)
-            global_config.set_state(sender_id,'state_init')
-            machine.go_to(event)
+        if event.get("message"):
+            text = event['message']['text']
+            sender_id = event['sender']['id']
+            send.send_start(sender_id, text)
         return 'OK'
 
+
 if __name__ == "__main__":
-    machine = TocMachine(
-        states = states_config.states,
-        transitions = states_config.transitions,
-        initial = 'state_init',
-        auto_transitions = False,
-        show_conditions = True
-    )
-    # machine.get_graph().draw('show-fsm.png', prog='dot')
     app.run(host="0.0.0.0", port=PORT, debug=True, reloader=True)
