59c9842960e45d21a015824ee14891e1a27606d8
yiiju <yijuhung0129@gmail.com>
2018-12-17 03:03:21 +0800

    Change back to real code

--- app.py
+++ app.py
@@ -1,10 +1,9 @@
 from bottle import Bottle, route, run, request, abort, static_file
 
+from fsm import TocMachine
 import states_config
 import global_config
 import os
-import send
-from fsm import TocMachine
 
 app = Bottle()
 
@@ -26,24 +25,31 @@ def setup_webhook():
 @app.route("/yiju", method="POST")
 def webhook_handler():
     body = request.json
-    print('REQUEST BODY: ')
-    print(body)
+    print('\nFSM STATE: ' + machine.state)
 
     if body['object'] == "page":
         event = body['entry'][0]['messaging'][0]
-        if event.get("message"):
-            text = event['message']['text']
-            sender_id = event['sender']['id']
-            send.send_start(sender_id, text)
+        sender_id = event['sender']['id']
+
+        if sender_id in global_config.id_list[0]:
+            global_config.get_state(sender_id)
+            if machine.state == 'state_init':
+                machine.go_to(event)
+            else:
+                machine.advance(event)
+        else:
+            global_config.set_id(sender_id)
+            global_config.set_state(sender_id,'state_init')
+            machine.go_to(event)
         return 'OK'
 
-
 if __name__ == "__main__":
-    # machine = TocMachine(
-    #     states = states_config.states,
-    #     transitions = states_config.transitions,
-    #     initial = 'state_init',
-    #     auto_transitions = False,
-    #     show_conditions = True
-    # )
+    machine = TocMachine(
+        states = states_config.states,
+        transitions = states_config.transitions,
+        initial = 'state_init',
+        auto_transitions = False,
+        show_conditions = True
+    )
+    machine.get_graph().draw('show-fsm.png', prog='dot')
     app.run(host="0.0.0.0", port=PORT, debug=True, reloader=True)
